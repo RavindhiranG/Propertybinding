b89f4d7f46907150a6846b63ab8d22c40b347aef
RavindhiranG <ravindhirang@gmail.com>
2024-04-24 16:41:00 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // property binding is used to pass data from the component class (component.ts) and setting the value of the given element in the user-end (component.html)
   title = 'Propertybinding';
+
+  name = "Ravindhiran";
+
+  //using function
+
+
+  getMax(a:number,b:number) {
+    return Math.max(a,b);
+  }
+
+  image = "https://miro.medium.com/v2/resize:fit:1149/1*hY6K0vVIWgAs85HJnhnKIw.png";
 }
